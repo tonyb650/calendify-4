1efa376fe751f39a1c76ec962495775fe4257698
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-17 16:11:03 -0700

    Add interface delaration for next-auth session

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthConfig } from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../lib/prisma";
+import { getUserById } from "./db/users";
 
 // This is the full instance of Auth.js that *uses* the config object
 // from auth.config.ts
@@ -20,22 +21,22 @@ const edgeConfig: NextAuthConfig = {
       })
     }
   },
-  // callbacks: {
-  //   async jwt({token}) {
-  //     const existingUser = await getUserById(token.sub)
-  //     if (!existingUser) return token
-  //     token.earliest = existingUser.earliest
-  //     token.latest = existingUser.latest
-  //     return token
-  //   },
-  //   async session({ token, session}) {
-  //     if (!session.user) return session
-  //     if (token.sub) session.user.id = token.sub
-  //     if (token.earliest) session.user.earliest = token.earliest as number
-  //     if (token.earliest ) session.user.latest = token.latest as number
-  //     return session
-  //   },
-  // },
+  callbacks: {
+    async jwt({token}) {
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+      token.earliest = existingUser.earliest
+      token.latest = existingUser.latest
+      return token
+    },
+    async session({ token, session}) {
+      if (!session.user) return session
+      if (token.sub) session.user.id = token.sub
+      if (token.earliest) session.user.earliest = token.earliest as number
+      if (token.earliest ) session.user.latest = token.latest as number
+      return session
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt"},
   ...authConfig
