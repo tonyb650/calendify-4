c244f99c2979a65f6666182ef7365706ab8b8d14
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-17 16:19:05 -0700

    Add more packages

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -11,35 +11,3 @@ declare module "next-auth" {
     user: AppUser;
   }
 }
-
-/* TODO Determine if this next declaration is necessary */
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     earliest?: number;
-//     latest?: number;
-//   }
-// }
-
-/*
-
-import NextAuth, { type DefaultSession } from "next-auth"
-
-declare module "next-auth" {
-  /**
-   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-  
-  interface Session {
-    user: {
-      /** The user's role. 
-      role: "ADMIN" | "USER"
-      /**
-       * By default, TypeScript merges new interface properties and overwrites existing ones.
-       * In this case, the default session user properties will be overwritten,
-       * with the new ones defined above. To keep the default session user properties,
-       * you need to add them back into the newly declared interface.
-       *
-    } & DefaultSession["user"]
-  }
-}
-
-*/
