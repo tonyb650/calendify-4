e0e584308923d0617886ae1020f85ffef821db16
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-17 15:20:25 -0700

    Another try

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import prisma from "../lib/prisma";
+
+// const prisma = new PrismaClient()
 
 export const { handlers: { GET, POST}, auth} = NextAuth({
   adapter: PrismaAdapter(prisma),
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,16 @@
+import NextAuth from "next-auth"
 import { auth } from "./auth"
+import authConfig from "./auth.config"
 
-export default auth((req) => { // Here's the magic. In this middleware, req.auth.user will contain our logged in user data
-	const isLoggedIn = !!req.auth
-  console.log(isLoggedIn)
-	// do stuff here
-})
+// export default auth((req) => { // Here's the magic. In this middleware, req.auth.user will contain our logged in user data
+// 	const isLoggedIn = !!req.auth
+//   console.log(isLoggedIn)
+// 	// do stuff here
+// })
 
+export const {auth: middleware} = NextAuth(authConfig)
 // Any path that is NOT matched will invoke the middleware above
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"], // <-- This matcher comes from Clerk, presenter says it is best matcher
 }
