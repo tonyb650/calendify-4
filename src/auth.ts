b454b00fa89feb4b28363ac8e2fa08224943fd3d
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-17 14:42:49 -0700

    Full auth.config.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
 import authConfig from "./auth.config";
 
 export const { handlers: { GET, POST}, auth} = NextAuth(authConfig)
