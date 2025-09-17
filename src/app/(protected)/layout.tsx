ef7b4a58ac7b400625fec2f465298d518c3d4196
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-17 16:37:59 -0700

    Everything except the calendar

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/app/(protected)/_components/NavBar";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
+
+const ProtectedLayout = async ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const session = await auth()
+
+  return (
+    <SessionProvider session={session}>
+      {/* <Suspense fallback="Loading..."> */}
+        {/* <AwaitedNavbar /> */}
+        <Navbar/>
+      {/* </Suspense> */}
+      <main className="max-w-7xl mx-auto">{children}</main>
+    </SessionProvider>
+  );
+}
+
+export default ProtectedLayout
