d0cfdc0260e7e8d5a8d9da8269c8397eb54a24ab
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-17 14:16:02 -0700

    minimum prisma again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
-'use client'
-
 import prisma from "../../lib/prisma";
 
 export default function Home() {
 
-  const handleClick = async () => {
-    console.log("attempting prisma call")
-    await prisma.verificationToken.create({data: {identifier: "1", token: "1", expires: new Date()}})
+  async function createTodo(formData: FormData) { 
+    "use server"
+    // This code is being called only on the server
+    console.log(formData)               // Will log form data on the server console
+    console.log(formData.get("title")) 
+    // ... await async database 'create' action here
+    await prisma.verificationToken.create({data:{identifier: "2", expires: new Date(), token: "3"}})
   }
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <button onClick={handleClick}>Prisma Now!</button>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      </footer>
-    </div>
-  );
+  
+	return (
+		<form
+			action={createTodo}         
+      >                               
+      {/* <-- action prop in HTML lets you pass the URL you want to go to, */} 
+      {/*	for example "/new/todo", but NextJS we can call a function that will run on the server (Next makes a fetch request) */}
+			<input type="text" name="title" id="title"/>
+			<button>Submit</button>
+		</form>
+	)
 }
+
+
